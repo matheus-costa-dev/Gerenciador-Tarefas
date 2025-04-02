753e356feb6522d300597da6dda1095fbf3b6518
matheus costa <dev.matheuspc@gmail.com>
2025-04-02 11:06:54 -0300

    alterado velocidade type speed to typewriter

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
   return (
     <Section >
         <h1 className='text-5xl lg:text-3xl bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent text-center'>
-         <Typewriter words={["TaskMaster","Gerenciador de Tarefas"]}/>
+         <Typewriter words={["TaskMaster","Gerenciador de Tarefas"]} typeSpeed={100} deleteSpeed={100}/>
         </h1>
         <button className='min-w-[200px] text-2xl lg:text-base w-1/3 rounded-2xl p-1 mx-auto bg-yellow-500 hover:bg-red-400' onClick={()=>{navigate("/login")}}>Acessar</button>
     </Section>
